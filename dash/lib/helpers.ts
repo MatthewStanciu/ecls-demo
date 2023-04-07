74a17f80d89aa02edacfefccebf0a07cf6c67752
Matthew Stanciu <mattbstanciu@gmail.com>
2023-04-07 18:00:15 -0400

    consistentize

--- dash/lib/helpers.ts
+++ dash/lib/helpers.ts
@@ -3,7 +3,7 @@ import { ConfigData } from "../types/types";
 const dev = process.env.NODE_ENV !== "production";
 export const server = dev
   ? "http://localhost:3000"
-  : `${process.env.NEXT_PUBLIC_DASH_API_BASE_URL}`;
+  : process.env.NEXT_PUBLIC_DASH_API_BASE_URL;
 
 export const fetcher = (url: string) =>
   fetch(url, {
